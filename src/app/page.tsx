02392b95625a4f912a02effc7e55ca76114faf93
ArturCassu <artur.cdj@gmail.com>
2025-04-13 11:50:02 -0300

    tipo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,33 +9,6 @@ import fallback from "@/app/assets/fallback.jpg"
 import logo from "@/app/assets/logo.jpg"
 import useWebSocket from "@/app/hooks/useWebSocket";
 
-export type UserGroupData = {
-    id: string;
-    name: string;
-    userIds: string[]
-    messages: MessageData[]
-    unread: boolean
-}
-export type MessageData = {
-    message: string;
-    type: MessageType;
-    timestamp: number;
-    name: string;
-    image: string|undefined;
-}
-
-export enum MessageType {
-    USER = "USER",
-    SYSTEM = "SYSTEM",
-}
-
-export type User = {
-    "id": string,
-    "name": string,
-    "image": string|undefined,
-}
-
-
 
 export default function Home() {
 
